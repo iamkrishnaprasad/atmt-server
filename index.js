dbe93e7f55b2cf09ac34a17c1698b24518e858cc
Krishna Prasad M <krishnaprasad1991@gmail.com>
2022-01-09 16:44:48 +0530

    changed default port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
 // const logger = require('./src/utils/logger-winston');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
